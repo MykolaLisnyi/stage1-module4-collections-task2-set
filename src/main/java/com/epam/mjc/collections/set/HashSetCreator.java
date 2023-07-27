5b7516e04040244cf9d4633112b256d80d77246a
Mykola Lisnyi <lesnoy.nikolay@gmail.com>
2023-07-27 19:38:35 +0300

    1

--- src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
+++ src/main/java/com/epam/mjc/collections/set/HashSetCreator.java
@@ -2,8 +2,17 @@ package com.epam.mjc.collections.set;
 
 import java.util.HashSet;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class HashSetCreator {
     public HashSet<Integer> createHashSet(List<Integer> sourceList) {
+        return sourceList.stream()
+                .flatMap(source -> source % 2 == 1
+                        ? Stream.of(source, source * 2)
+                        : Stream.iterate(source, s -> s % 2 == 0, s -> s / 2)
+                                .flatMap(s -> Stream.of(s, s / 2))
+                                .distinct())
+                .collect(Collectors.toCollection(HashSet::new));
     }
 }
--- src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
+++ src/main/java/com/epam/mjc/collections/set/SetCombinationCreator.java
@@ -5,5 +5,16 @@ import java.util.Set;
 
 public class SetCombinationCreator {
     public Set<String> createSetCombination(Set<String> firstSet, Set<String> secondSet, Set<String> thirdSet) {
+        Set<String> fourthSet = new HashSet<>(firstSet);
+        fourthSet.retainAll(secondSet);
+        fourthSet.removeAll(thirdSet);
+
+        Set<String> resulthSet = new HashSet<>(thirdSet);
+        resulthSet.removeAll(firstSet);
+        resulthSet.removeAll(secondSet);
+
+        resulthSet.addAll(fourthSet);
+
+        return resulthSet;
     }
 }
